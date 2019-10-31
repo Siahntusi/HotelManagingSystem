4514a06be7a79af2bd5745c48fed043e16369119
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 21:13:41 +0200

    Client class

---
+++ Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_SERVICE_CLIENT
+{
+   [DataContract]
+    class Client :BaseUser
+    {
+        private string paymentType;
+        private int cityID;
+
+        [DataMember(Name = "PaymentType")]
+        public string PaymentType
+        {
+            get { return paymentType; }
+            set { paymentType = value; }
+        }
+
+        [DataMember(Name = "CityID")]
+        public int CityID
+        {
+            get { return cityID; }
+            set { cityID = value; }
+        }
+    }
+}
