4a972f89f67d2c509541a1f6f9bcff8f7c73e7d4
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 22:45:28 +0200

    For getting the uploaded files

---
+++ GetFile.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GetFile.aspx.cs" Inherits="AAFS.GetFile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ GetFile.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WCF_SERVICE_CLIENT_HOST.Models;
+
+namespace AAFS
+{
+    public partial class GetFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void downloadFile(ApplicationFile file)
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = file.ContentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + file.FileName);
+            //Response.BinaryWrite(file.data);
+            Response.Flush();
+            Response.End();
+        }
+
+        private void downloadImage(ImageFile image)
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = image.ContentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + image.ImageName);
+            //Response.BinaryWrite(image.Data);
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
