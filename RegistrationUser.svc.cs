59aa57df36d1cf062a985fb412ac007576db31ab
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 21:12:51 +0200

    Add files via upload

---
+++ RegistrationUser.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WFC
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RegistrationUser" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RegistrationUser.svc or RegistrationUser.svc.cs at the Solution Explorer and start debugging.
+    public class RegistrationUser : IRegistrationUser
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
