eadf5761da8b81959913566d45df7254ea80256f
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 21:14:52 +0200

    Manager class

---
+++ Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_SERVICE_CLIENT
+{
+    [DataContract]
+    class Manager : BaseUser
+    {
+        private int employeeNumber;
+
+        [DataMember(Name = "EmployeeNumber")]
+        public int EmployeeNumber
+        {
+            get { return employeeNumber; }
+            set { employeeNumber = value; }
+        }
+    }
+}
