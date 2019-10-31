726d8782e6de7e2ac78d10982bc5a25e486251c4
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 21:44:12 +0200

    Client moving in to the hotel acceptance

---
+++ Acc_AcceptMoveInReq.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Acc_AcceptMoveInReq.aspx.cs" Inherits="AAFS.Acc_AcceptMoveInReq" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+
+</asp:Content>
---
+++ Acc_AcceptMoveInReq.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WCF_SERVICE_CLIENT_HOST;
+using WCF_SERVICE_CLIENT_HOST.Models;
+
+namespace AAFS
+{
+    public partial class Acc_AcceptMoveInReq : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string querystrAccId = Request.QueryString["AccommID"].ToString();
+            string queryStrStd = Request.QueryString["StdId"].ToString();
+            BookingsServicesClient bookServ = new BookingsServicesClient();
+            Booking_Requests bookReq = new Booking_Requests();
+            bookReq.AccommoId = Convert.ToInt32(querystrAccId);
+            bookReq.BookingStatus = "APPROVED";
+            bookReq.StudId = Convert.ToInt32(queryStrStd);
+            string result = bookServ.updateMoveIn(bookReq);
+
+            System.Threading.Thread.Sleep(2000);
+            if (result.Contains("Success"))
+            {
+                Response.Redirect("Acc_ViewIndiviAccMoveInReq.aspx?Reslt=Success");
+            }
+            if (result.Contains("Failed"))
+            {
+                Response.Redirect("Acc_ViewIndiviAccMoveInReq.aspx?Reslt=Failed");
+            }
+
+
+        }
+    }
+}
