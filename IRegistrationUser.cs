1c86e499e7448c4168e733c6959b84f20db745d9
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 21:11:00 +0200

    Interface for user registration

---
+++ IRegistrationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCF_SERVICE_CLIENT;
+
+namespace WFC
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRegistrationUser" in both code and config file together.
+    [ServiceContract]
+    public interface IRegistrationUser
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST")]
+        string RegisterClient(Client client);
+    }
+}
