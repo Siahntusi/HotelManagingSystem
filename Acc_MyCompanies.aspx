064265ff5a8512797d6150700a4f61f543b2dbf4
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-10-31 21:47:27 +0200

    Companies that have hotels for hotel owners

---
+++ Acc_MyCompanies.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Acc_MyCompanies.aspx.cs" Inherits="AAFS.AccMyCompanies" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <style>
+        .Formlabel {
+            text-align: left;
+            display: inline;
+            display: block;
+        }
+
+        .auto-style1 {
+            height: 143px;
+        }
+
+        #content {
+            margin: 15px auto;
+            text-align: center;
+            width: 700px;
+            position: relative;
+            height: 100%;
+        }
+
+        #wrapper {
+            -moz-box-shadow: 0px 0px 3px #aaa;
+            -webkit-box-shadow: 0px 0px 3px #aaa;
+            box-shadow: 0px 0px 3px #aaa;
+            -moz-border-radius: 10px;
+            -webkit-border-radius: 10px;
+            border-radius: 10px;
+            border: 2px solid #fff;
+            background-color: #f9f9f9;
+            width: 700px;
+            overflow: hidden;
+        }
+
+        #steps {
+            overflow: hidden;
+            width: 700px;
+        }
+
+        .step {
+            float: left;
+            width: 700px;
+        }
+
+        .MoveBtn {
+            padding-right: 10px;
+            width: 100px;
+            margin: 15px;
+        }
+
+        .hidden {
+            display: none;
+        }
+    </style>
+
+    <div id="content" class="padding50">
+        <h1></h1>
+
+        <div id="wrapper">
+            <legend>
+                <h1>Manage Companies</h1>
+            </legend>
+            <div style="float: right; padding: 1%;">
+                <asp:Button ID="btnAddNewCompany" runat="server" Text="Add New Company" CssClass="btn btn-default" OnClick="btnAddNewCompany_Click" />
+            </div>
+
+            <div id="MyCompListDiv" runat="server" class=" table-responsive" style="width: 100%">
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
---
+++ Acc_MyCompanies.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WCF_SERVICE_CLIENT_HOST;
+using WCF_SERVICE_CLIENT_HOST.Models;
+
+namespace AAFS
+{
+    public partial class AccMyCompanies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string OwnrID = Session["ID"].ToString();
+            OwnerServicesClient ownrServClnt = new OwnerServicesClient();
+            List<OwnersCompany> ownrCompList = new List<OwnersCompany>();
+
+            ownrCompList = ownrServClnt.getCompByOwnerId(OwnrID);
+             string CompanyList = "";
+
+
+                CompanyList += "<table class='table table-striped'>";
+                //CompanyList += "<caption>Accommodations</caption>";
+                CompanyList += "<tr>";
+
+                CompanyList += "<th>" + "Company Name";
+                CompanyList += "</th>";
+
+                CompanyList += "<th >" + "Company Registration Number ";
+                CompanyList += "</th>";
+
+                CompanyList += "<th >" + "Telephone number";
+                CompanyList += "</th>";
+
+                CompanyList += "<th >" + "Email";
+                CompanyList += "</th>";
+
+                CompanyList += "<th >" + " Edit Company Details";
+                CompanyList += "</th>";
+
+                CompanyList += "</tr>";
+
+                foreach (OwnersCompany myCompany in ownrCompList)
+                {
+                    CompanyList += "<tr>";
+
+                    CompanyList += "<th>" + myCompany.CompanyName;
+                    CompanyList += "</th>";
+
+                    CompanyList += "<td>" + myCompany.RegNum;
+                    CompanyList += "</td>";
+
+                    CompanyList += "<td>" + myCompany.ContactNum;
+                    CompanyList += "</td>";
+
+                    CompanyList += "<td>" + myCompany.Email;
+                    CompanyList += "</td>";
+
+                    CompanyList += "<td>";
+                    CompanyList += "<a class='btn btn-info' style='color: #000;width:80%;' href='Acc_EditCompanyDet.aspx?CompID=" + myCompany.CompId + "'>Update</a>";
+                    CompanyList += "</td>";
+
+                    CompanyList += "</tr>";
+
+                }
+                CompanyList += "</Table>";
+
+                MyCompListDiv.InnerHtml = CompanyList;
+            }
+
+        protected void btnAddNewCompany_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Acc_AddCompany.aspx");
+        }
+
+     
+    }
+}
