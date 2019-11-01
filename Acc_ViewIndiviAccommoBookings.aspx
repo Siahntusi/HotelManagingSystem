c563ccea99a219ef7ab9f723a6b5c7b226fc3003
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-11-01 02:07:50 +0200

    Individual Accommodations

---
+++ Acc_ViewIndiviAccommoBookings.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Acc_ViewIndiviAccommoBookings.aspx.cs" Inherits="AAFS.Acc_ViewIndiviAccommoBookings" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+     <style>
+        td {
+            text-align: left;
+        }
+
+        #content {
+            margin: 15px auto;
+            text-align: center;
+            width: 850px;
+            position: relative;
+            height: 100%;
+            top: 0px;
+            left: 0px;
+        }
+
+        #wrapper {
+            -moz-box-shadow: 0px 0px 3px #aaa;
+            -webkit-box-shadow: 0px 0px 3px #aaa;
+            box-shadow: 0px 0px 3px #aaa;
+            -moz-border-radius: 10px;
+            -webkit-border-radius: 10px;
+            border-radius: 10px;
+            border: 2px solid #fff;
+            background-color: #f9f9f9;
+            width: 850px;
+            overflow: hidden;
+        }
+    </style>
+
+    <div id="content" class="padding50">
+        <h1></h1>
+
+        <div id="wrapper">
+            <legend>
+                <h1><asp:Label ID="lblNAme" runat="server" Text=""></asp:Label></h1>
+            </legend>
+
+            <%--<div id="SelectUsr" style="width:50%; margin-left:0;margin-right:0;text-align:center">
+                <asp:DropDownList ID="dropdwnUsers" runat="server" CssClass="form-control" AppendDataBoundItems="true" AutoPostBack="true" OnSelectedIndexChanged="dropdwnUsers_SelectedIndexChanged" ToolTip="Select Gender" style="width:50%; margin-left:0;margin-right:0;">
+                    <asp:ListItem Value="-1" disabled="disabled">Select Users </asp:ListItem>
+                    <asp:ListItem Value="1">All</asp:ListItem>
+                    <asp:ListItem Value="2">Officers</asp:ListItem>
+                    <asp:ListItem Value="3">Students</asp:ListItem>
+                    <asp:ListItem Value="4">Owners</asp:ListItem>
+                </asp:DropDownList>
+
+            </div>--%>
+
+            <div id="AccommoStudBookingsDiv" runat="server" class="  table table-responsive" style="width: 100%">
+            </div>
+           
+
+        </div>
+    </div>
+</asp:Content>
---
+++ Acc_ViewIndiviAccommoBookings.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WCF_SERVICE_CLIENT_HOST;
+using WCF_SERVICE_CLIENT_HOST.Models;
+
+namespace AAFS
+{
+    public partial class Acc_ViewIndiviAccommoBookings : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HotelServicesClient accServClnt = new HotelServicesClient();
+            BookingsServicesClient bookingServClnt = new BookingsServicesClient();
+            Hotel Accommo = new Hotel();
+            string quearyString = Request.QueryString["AccommID"];
+            Accommo = accServClnt.getHotelFullInfoById(quearyString);
+            List<ClientBooksHotel> studBookList = new List<ClientBooksHotel>();
+            bookingServClnt.checkForInvalidBookingsForHotel(quearyString);
+            studBookList = bookingServClnt.getAllBookingsMadeToHotel(quearyString);
+
+            lblNAme.Text = Accommo.HotelName + " Student Bookings";
+
+            String AccommodationsList = "";
+
+
+
+            if (studBookList != null)
+            {
+                AccommodationsList += "<table class='table table-striped'>";
+                //AccommodationsList += "<caption>Accommodations</caption>";
+                AccommodationsList += "<tr>";
+
+                AccommodationsList += "<th>" + "Student Name(s) & Surname";
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "<th >" + "Contact Number";
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "<th >" + "Date Booked";
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "<th >" + "Booking Expiry";
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "<th >" + "Funding Type";
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "<th >" + "Booking Status";
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "</tr>";
+            
+            foreach (ClientBooksHotel studBook in studBookList)
+            {
+                AccommodationsList += "<tr>";
+
+                AccommodationsList += "<th>" + studBook.Client.Name + " "+ studBook.Client.Surname;
+                AccommodationsList += "</th>";
+
+                AccommodationsList += "<td>" + studBook.Client.ContactNumber;
+                AccommodationsList += "</td>";
+
+                AccommodationsList += "<td>" + studBook.TimeStamp;
+                AccommodationsList += "</td>";
+
+                AccommodationsList += "<td>" + studBook.BookingDuration;
+                AccommodationsList += "</td>";
+
+                AccommodationsList += "<td>" + studBook.Client.FundingType;
+                AccommodationsList += "</td>";
+
+                AccommodationsList += "<td>" + studBook.BookingStatus;
+                AccommodationsList += "</td>";
+
+                AccommodationsList += "</tr>";
+            }
+            AccommodationsList += "</Table>";
+
+            
+            }
+            else
+            {
+                AccommodationsList += "<p class='alert alert-info'><big><strong>No Bookings</strong></big></p>";
+            }
+            AccommoStudBookingsDiv.InnerHtml = AccommodationsList;
+        }
+
+    }
+}
