33bea0c2d6d84655760c1caf8be1f568fc6a63cd
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-11-01 06:00:13 +0200

    Accreditation Entities

---
+++ AccreditationApplications.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCF_SERVICE_CLIENT_HOST.Models
+{
+    public class AccreditationApplications
+    {
+        public Accommodation accommo
+        {
+            set;
+            get;
+        }
+
+        public List<ApplicationFile> ApplicationFiles
+        {
+            set;
+            get;
+        }
+
+        public int ApplicationId
+        {
+            set;
+            get;
+        }
+
+        public int AccommoId
+        {
+            set;
+            get;
+        }
+
+        public string ApplicationDate
+        {
+            set;
+            get;
+        }
+
+        public string ReferenceNumber
+        {
+            set;
+            get;
+        }
+
+        public string ApplicationStatus
+        {
+            set;
+            get;
+        }
+
+        public string Reason
+        {
+            set;
+            get;
+        }
+    }
+}
---
+++ AccreditationDuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_SERVICE_CLIENT_HOST.Models
+{
+    public class AccreditationDuration
+    {
+        public static string computeDuration()
+        {
+            string start = DateTime.Now.AddYears(1).Year.ToString() + "-01-01";
+            DateTime end = Convert.ToDateTime(start).AddYears(1);
+
+            return end.ToString("yyyy-mm-dd");
+        }
+    }
+}
