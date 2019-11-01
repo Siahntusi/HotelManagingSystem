b856b435131aa437bc3c6515650b266b268f7dcf
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-11-01 06:00:52 +0200

    Longitude and Latitude entities

---
+++ Address.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+
+namespace WCF_SERVICE_CLIENT_HOST.Models
+{
+    [DataContract]
+    public class Address
+    {
+        private int addressID;
+        private string street;
+        private string town;
+        private string city;
+        private int postalCode;
+        private string longitude;
+        private string lattitude;
+        private string infoUrl;
+
+        [DataMember]
+        public string InfoUrl
+        {
+            get { return infoUrl; }
+            set { infoUrl = value; }
+        }
+
+        [DataMember]
+        public string Lattitude
+        {
+            get { return lattitude; }
+            set { lattitude = value; }
+        }
+
+        [DataMember]
+        public string Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+
+        [DataMember]
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        [DataMember]
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+
+        [DataMember]
+        public int PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+
+        [DataMember]
+        public int AddressID
+        {
+            get { return addressID; }
+            set { addressID = value; }
+        }
+
+        [DataMember]
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+    }
+}
