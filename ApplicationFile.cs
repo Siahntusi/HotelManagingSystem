6732cbce49c7d96bc9c28bd44e0a401ea0561a5a
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-11-01 06:01:29 +0200

    Application files entities

---
+++ ApplicationFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCF_SERVICE_CLIENT_HOST.Models
+{
+    public class ApplicationFile
+    {
+        public int FileId
+        {
+            get;
+            set;
+        }
+
+        public int ApplicationId
+        {
+            get;
+            set;
+        }
+
+        public string Location
+        {
+            get;
+            set;
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public string FileCategory
+        {
+            get;
+            set;
+        }
+
+        public string ContentType
+        {
+            get;
+            set;
+        }
+
+        public long FileSize
+        {
+            get;
+            set;
+        }
+
+        //public byte[] data
+        //{
+        //    get;
+        //    set;
+        //}
+
+        public string DateUploaded
+        {
+            get;
+            set;
+        }
+    }
+}
