fcc53baed6173cf22797b458031500c7297df26a
Siahntusi <43526003+Siahntusi@users.noreply.github.com>
2019-11-01 05:58:03 +0200

    Status codes

---
+++ Codes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_SERVICE_CLIENT_HOST.Models.StatusCodes
+{
+    public enum BookingCodes { INVALID, VALID, REQ_MOVE_IN, APPROVED, CANCELLED, NOT_APPROVED}
+    public enum AccreditationCodes {APPROVED, DISAPPROVED, PENDING, NOT_ACCREDITTED, ACCREDITTED}
+    public enum InspectionCodes {PASSED, FAILED, PENDING}
+
+    class Codes
+    {
+    }
+}
